6300b4b34dbf10889809bf8a6871fc9f408b8174
KANISHK RAJ <kanishkraj601@gmail.com>
2025-01-22 07:33:09 +0000

    1765. Map of Highest Peak

---
+++ Breadth-First Search/1765_Map_of_Highest_Peak.cpp
@@ -0,0 +1,41 @@
+class Solution {
+
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        int m = isWater.size();
+        int n = isWater[0].size();
+        vector<vector<int>> height(m, vector<int>(n, -1));
+        queue<pair<int, int>> q;
+        
+       
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (isWater[i][j] == 1) {
+                    height[i][j] = 0;
+                    q.push({i, j});
+                }
+            }
+        }
+
+
+        vector<int> dx = {0, 0, 1, -1};
+        vector<int> dy = {1, -1, 0, 0};
+
+        while (!q.empty()) {
+            auto [x, y] = q.front();
+            q.pop();
+
+            for (int i = 0; i < 4; ++i) {
+                int newX = x + dx[i];
+                int newY = y + dy[i];
+
+                if (newX >= 0 && newX < m && newY >= 0 && newY < n && height[newX][newY] == -1) {
+                    height[newX][newY] = height[x][y] + 1;
+                    q.push({newX, newY});
+                }
+            }
+        }
+
+        return height;
+    }
+};
