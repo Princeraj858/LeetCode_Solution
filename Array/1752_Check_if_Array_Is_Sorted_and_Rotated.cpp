9b1693e26675d6051752320fb5c96ee75d99be52
KANISHK RAJ <kanishkraj601@gmail.com>
2025-02-02 13:28:05 +0000

    1752. Check if Array Is Sorted and Rotated

---
+++ Array/1752_Check_if_Array_Is_Sorted_and_Rotated.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n = nums.size();
+
+        int pos = 1;
+        while(pos < n and nums[pos] >= nums[pos-1])
+            pos++;
+        if(pos==n)      return true;
+        if(nums[pos]>nums[0])    return false;
+        if(pos==n-1)    return true;
+
+        pos+=1;
+        while(pos < n and nums[pos]<=nums[0] and nums[pos]>=nums[pos-1])
+            pos++;
+        return pos==n;
+    }
+};
